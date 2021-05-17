5649a08a0eb24cb85bdacaa24bd0e40bfeedb475
Ritam Jana <ritamjana2002@gmail.com>
2021-05-17 12:39:10 +0530

    navbar modified. CSS changed to SASS

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.scss';
+import Navbar from './components/Navbar/Navbar';
+import {BrowserRouter as Router} from "react-router-dom"
+
+function App() {
+  return (
+    <Router>
+      <Navbar/>
+    </Router>
+  );
+}
+
+export default App;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,39 @@
-import React from "react";
+import React from "react"
+import {FaBars} from "react-icons/fa"
+import "./Navstyle.scss";
+import { Link as LinkR } from "react-router-dom"
+import {Link as LinkS} from "react-scroll"
 
 const Navbar = () => {
-  return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
-  );
-};
+	return (
+		<>
+			<nav>
+				<div className="container">
+					<LinkR  to="/" className="brandName">Ritam Jana</LinkR>
+					<div className="mobileIcons">
+						<FaBars />
+					</div>
+					<ul className="navMenu">
+						<li className="navItem">
+							<LinkS to="about" className="navLinks">About</LinkS>
+						</li>
+						<li className="navItem">
+							<LinkS to="experience" className="navLinks">Experience</LinkS>
+						</li>
+						<li className="navItem">
+							<LinkS to="projects" className="navLinks">Projects</LinkS>
+						</li>
+						<li className="navItem">
+							<LinkS to="contact" className="navLinks">Contact</LinkS>
+						</li>
+					</ul>
+					<nav className="resumeBtn">
+						<LinkR to="/resume" className="resumeBtnLink">Resume</LinkR>
+					</nav>
+				</div>
+			</nav>
+		</>
+	)
+}
 
-export default Navbar;
+export default Navbar
